1449c5149bd1d43c937bce96a3e1e591ee860b20
Zola <zolamcadie@gmail.com>
2017-07-25 23:45:02 +0000

    initialized server, recieving messages from app

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const SocketServer = require('ws').Server;
+
+const PORT = 3001;
+const server = express()
+   // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Server listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+// Set up a callback that will run when a client connects to the server
+// When a client connects they are assigned a socket, represented by
+// the ws parameter in the callback.
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.onmessage = (event) => {
+    console.log(event.data);
+  };
+
+  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+  ws.on('close', () => console.log('Client disconnected'));
+});
