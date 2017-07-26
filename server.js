8f3ab6af2c42a901ad5ca98c5244701dfdeca56a
Zola <zolamcadie@gmail.com>
2017-07-26 00:50:24 +0000

    added uuid functionality and basic message framework

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
+const uuidv4 = require('uuid/v4');
 
 const PORT = 3001;
 const server = express()
@@ -8,6 +9,15 @@ const server = express()
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Server listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
+
+const buildMessage = (message) => {
+  const newMessage = {
+    id: uuidv4(),
+    type: message.type,
+    username: message.username,
+    content: message.content
+  }
+}
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
